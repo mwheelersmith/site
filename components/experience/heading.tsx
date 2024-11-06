6a20433ddb19fe9146975e23c2027e5c345b17ca
Miles Wheeler-Smith <miles@mileswheelersmith.com>
2024-11-06 22:19:29 +0000

    Fix TypeScript children prop issue

--- components/experience/heading.tsx
+++ components/experience/heading.tsx
@@ -1,3 +1,5 @@
-export function ExperienceHeading({ children }) {
+import { ReactNode } from "react";
+
+export function ExperienceHeading({ children }: { children: ReactNode }) {
   return <div className="flex flex-col">{children}</div>;
 }

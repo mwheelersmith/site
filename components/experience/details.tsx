eba829ebd872d3b6c83087ed910cbb391b8f4ef4
Miles Wheeler-Smith <miles@mileswheelersmith.com>
2024-11-06 22:10:34 +0000

    Fix service length breaking mobile

--- components/experience/details.tsx
+++ components/experience/details.tsx
@@ -18,7 +18,7 @@ export function ExperienceDetails({
       <p className="flex items-center gap-2">
         <Calendar size={16} />
         {serviceDates}
-        <span className="text-sm text-zinc-500 dark:text-zinc-400">
+        <span className="hidden text-sm text-zinc-500 sm:inline-block dark:text-zinc-400">
           &bull; {serviceLength}
         </span>
       </p>

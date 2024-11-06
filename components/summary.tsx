374d1cf320e61a6e099306b711cd68493a50c8ca
Miles Wheeler-Smith <miles@mileswheelersmith.com>
2024-11-06 16:40:14 +0000

    Add a note to view the site on GitHub

--- components/summary.tsx
+++ components/summary.tsx
@@ -2,7 +2,6 @@ export function Summary() {
   return (
     <div className="flex flex-col gap-6 border-b border-gray-200 pb-8">
       <h2 className="text-xl font-medium">Summary</h2>
-
       <p>
         Hi, I&rsquo;m a front-end engineer, software architect, and team lead.
       </p>
@@ -10,6 +9,18 @@ export function Summary() {
         I began my journey in web development over 15 years ago as a full-stack
         PHP developer, and have since evolved into a specialised UI engineering
         leader.
+      </p>
+      <p className="print:hidden">
+        P.S - This site is available to view on{" "}
+        <a
+          className="text-indigo-800 hover:underline hover:underline-offset-4"
+          href="https://github.com/mwheelersmith/site"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+        .
       </p>
     </div>
   );

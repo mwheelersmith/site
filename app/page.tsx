ff87b5086d4724ff39e4fb444b2ccfefd3f34312
Miles Wheeler-Smith <miles@mileswheelersmith.com>
2024-11-06 21:52:21 +0000

    Fix mobile view and add more experiences

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ import { ExperienceContributionsItem } from "@/components/experience/contributio
 
 export default function Home() {
   return (
-    <main className="mx-auto my-4 flex flex-col gap-8 rounded-lg bg-white p-8 font-[family-name:var(--font-geist-sans)] shadow-md sm:p-16 lg:max-w-[1024px] dark:bg-zinc-900 dark:shadow-none">
+    <main className="mx-auto flex flex-col gap-8 rounded-lg bg-white p-8 font-[family-name:var(--font-geist-sans)] sm:p-16 lg:my-4 lg:max-w-[1024px] lg:shadow-md dark:bg-zinc-900 dark:shadow-none">
       <Header />
 
       <Summary />
@@ -32,10 +32,10 @@ export default function Home() {
           </ExperienceHeading>
 
           <p>
-            Responsible for architecting and building a multi-channel messaging
-            application for the Webex CPaaS suite. I led a team of five
-            experienced UI engineers, serving as both a senior engineer within
-            the team and the primary liaison for product managers, designers,
+            Resonisible for architecting and building a multi-channel messaging
+            application for the Webex CPaaS suite. I recruited and led a team of
+            five skilled UI engineers, contributing as a senior engineer while
+            serving as the primary liaison for product managers, designers,
             directors, and other key stakeholders.
           </p>
 
@@ -91,6 +91,84 @@ export default function Home() {
             comprehensive multi-channel solution. At this stage, I was promoted
             to UI Engineering Lead to oversee its development and successful
             delivery.
+          </p>
+        </ExperienceSection>
+
+        <ExperienceSection>
+          {/* UX Design Lead at Textlocal */}
+          <ExperienceHeading>
+            <ExperienceTitle>UX Design Lead</ExperienceTitle>
+            <ExperienceDetails
+              company="Textlocal"
+              serviceDates="Sep 16 - Aug 21"
+              serviceLength="5 yrs"
+            />
+          </ExperienceHeading>
+
+          <p>
+            Led a small front-end team for an award-winning SMS platform,
+            overseeing the redesign and implementation of the user interface to
+            align with modern UX best practices, enhancing usability and visual
+            coherence. Worked closely with back-end engineers to design APIs
+            that met stakeholder business needs and contributed to implementing
+            a new SMS engine powered by WebSockets to enable real-time
+            messaging.
+          </p>
+
+          <p>
+            Recruited and mentored a new front-end developer, providing hands-on
+            support and training to strengthen the team&rsquo;s capabilities and
+            foster growth.
+          </p>
+        </ExperienceSection>
+
+        <ExperienceSection>
+          {/* Full Stack Engineer at Textlocal */}
+          <ExperienceHeading>
+            <ExperienceTitle>Full Stack Engineer</ExperienceTitle>
+            <ExperienceDetails
+              company="Textlocal"
+              serviceDates="Jan 12 - Sep 16"
+              serviceLength="4 yrs 9 mos"
+            />
+          </ExperienceHeading>
+
+          <p>
+            Designed and developed web services to meet both internal and
+            external requirements, while providing technical support to a large,
+            diverse customer base. Working in a small team required flexibility
+            across the full stack—from UX design through to front-end and
+            back-end development—implementing new features and platform
+            improvements for an audience of over 160,000 multinational users.
+          </p>
+
+          <p>
+            Co-led the development and integration of two new products,
+            including a ticketing tool and a survey solution for businesses and
+            individuals, each closely integrated with the core SMS platform. The
+            ticketing tool earned two prestigious industry awards, outperforming
+            entries from major brands such as John Lewis and Audi.
+          </p>
+        </ExperienceSection>
+
+        <ExperienceSection>
+          {/* Web Developer at Bluelinemedia */}
+          <ExperienceHeading>
+            <ExperienceTitle>Web Developer</ExperienceTitle>
+            <ExperienceDetails
+              company="Bluelinemedia"
+              serviceDates="Nov 09 - Jan 12"
+              serviceLength="2 yrs 3 mos"
+            />
+          </ExperienceHeading>
+
+          <p>
+            As a Web Developer at a small agency, I developed a wide range of
+            websites for various clients, from simple brochure sites to complex
+            e-commerce and CMS-driven platforms. Gained both front-end and
+            back-end experience, and frequently managed client relationships,
+            ensuring their needs were met throughout the website development
+            process.
           </p>
         </ExperienceSection>
       </Experience>
--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ export function Header() {
     <div className="flex flex-col gap-2 border-b border-zinc-200 pb-8 dark:border-zinc-600">
       <h1 className="text-2xl font-medium">Miles Wheeler-Smith</h1>
 
-      <div className="flex gap-6">
+      <div className="flex flex-wrap gap-x-6 gap-y-2">
         <a
           className="flex items-center gap-2 text-indigo-800 hover:underline hover:underline-offset-4 dark:text-indigo-300"
           href="mailto:miles@mileswheelersmith.com"

2755014fe8536371c1efb13b139e4f0cd1cefd23
Miles Wheeler-Smith <miles@mileswheelersmith.com>
2024-11-12 10:26:50 +0000

    Fix homepage mobile spacing

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { LinksListItem } from "@/components/links-list-item";
 
 export default function Home() {
   return (
-    <main className="mx-auto mt-32 flex flex-col md:w-[640px]">
+    <main className="mx-auto flex flex-col md:mt-32 md:w-[640px]">
       <div className="flex flex-col gap-16 rounded-xl border-zinc-200 p-16 pb-8 md:border dark:border-zinc-800">
         <Image
           className="self-center rounded-full"

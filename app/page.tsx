755a6c915cc9cb86521b08b6835d2fdbecd58391
Miles Wheeler-Smith <miles@mileswheelersmith.com>
2024-11-14 13:30:25 +0000

    Update profile picture

--- app/page.tsx
+++ app/page.tsx
@@ -6,30 +6,32 @@ import { LinksListItem } from "@/components/links-list-item";
 
 export default function Home() {
   return (
-    <main className="mx-auto flex flex-col md:w-[640px]">
-      <div className="flex flex-col gap-16 rounded-xl border-zinc-200 p-16 pb-8 md:border dark:border-zinc-800">
+    <main className="mx-2 flex flex-col md:mx-auto md:w-[640px]">
+      <div className="flex flex-col gap-16 overflow-hidden rounded-xl border-zinc-200 pb-8 md:border dark:border-zinc-800">
         <Image
-          className="self-center rounded-full"
-          src="/miles-avatar.jpg"
-          width={256}
-          height={256}
+          className="self-center rounded-xl md:rounded-none"
+          src="/miles-banner.jpg"
+          width={640}
+          height={427}
           alt="Miles smiling by a sunlit swimming pool."
         />
 
-        <Hello />
+        <div className="flex flex-col gap-16 px-8 sm:px-16">
+          <Hello />
 
-        <LinksList>
-          <LinksListItem href="https://github.com/mwheelersmith">
-            GitHub
-          </LinksListItem>
-          <LinksListItem href="https://www.linkedin.com/in/mwheelersmith">
-            LinkedIn
-          </LinksListItem>
-          <LinksListItem href="https://x.com/mwheelersmith">X</LinksListItem>
-          <LinksListItem href="https://cal.com/mwheelersmith">
-            Schedule a Call
-          </LinksListItem>
-        </LinksList>
+          <LinksList>
+            <LinksListItem href="https://github.com/mwheelersmith">
+              GitHub
+            </LinksListItem>
+            <LinksListItem href="https://www.linkedin.com/in/mwheelersmith">
+              LinkedIn
+            </LinksListItem>
+            <LinksListItem href="https://x.com/mwheelersmith">X</LinksListItem>
+            <LinksListItem href="https://cal.com/mwheelersmith">
+              Schedule a Call
+            </LinksListItem>
+          </LinksList>
+        </div>
       </div>
     </main>
   );
